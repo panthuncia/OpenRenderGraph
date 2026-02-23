01cd0ebd71003d8a2a86bbaff79e17d7deef70fa
Panthuncia <matthew.h.gomes@gmail.com>
2026-02-23 17:46:10 -0500

    one more uint, one fewer resource descriptor index

--- include/Render/ShaderAPI.h
+++ include/Render/ShaderAPI.h
@@ -4,7 +4,7 @@
 
 namespace rg::shaderapi {
 	inline constexpr uint32_t kResourceDescriptorIndicesRootParameter = 6;
-	inline constexpr uint32_t kNumResourceDescriptorIndicesRootConstants = 35;
+	inline constexpr uint32_t kNumResourceDescriptorIndicesRootConstants = 34;
 
 	inline constexpr uint32_t kIndirectCommandSignatureRootParameter = 7;
 	inline constexpr uint32_t kNumIndirectCommandSignatureRootConstants = 4;
