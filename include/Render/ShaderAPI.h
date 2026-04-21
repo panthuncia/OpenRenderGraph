3f2f988ae2dd75b71300be801b3b8f90705dbbb9
Panthuncia <matthew.h.gomes@gmail.com>
2026-04-21 01:48:19 -0400

    More rdi constants

--- include/Render/ShaderAPI.h
+++ include/Render/ShaderAPI.h
@@ -4,7 +4,7 @@
 
 namespace rg::shaderapi {
 	inline constexpr uint32_t kResourceDescriptorIndicesRootParameter = 5;
-	inline constexpr uint32_t kNumResourceDescriptorIndicesRootConstants = 31;
+	inline constexpr uint32_t kNumResourceDescriptorIndicesRootConstants = 64;
 
 	inline constexpr uint32_t kIndirectCommandSignatureRootParameter = 6;
 	inline constexpr uint32_t kNumIndirectCommandSignatureRootConstants = 4;
